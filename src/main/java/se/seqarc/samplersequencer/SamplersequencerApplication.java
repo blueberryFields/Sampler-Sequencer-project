14d7878e9db87349160c47db0ce7bc7a12f50c55
Adam Hambe <adamhambe@gmail.com>
2019-11-05 14:53:10 +0100

    Initial commit

---
+++ src/main/java/se/seqarc/samplersequencer/SamplersequencerApplication.java
@@ -0,0 +1,13 @@
+package se.seqarc.samplersequencer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamplersequencerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SamplersequencerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/seqarc/samplersequencer/SamplersequencerApplicationTests.java
@@ -0,0 +1,13 @@
+package se.seqarc.samplersequencer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamplersequencerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
