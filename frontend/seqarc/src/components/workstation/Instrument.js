a4ae207010caedf5b87788efd8593f4616e329f9
SebastianKihlstrand <sebastian.kihlstran@hotmail.com>
2019-11-19 13:09:48 +0100

    Issue#12 Add more stuff to Instrument.js and create Step.js

--- frontend/seqarc/src/components/workstation/Instrument.js
+++ frontend/seqarc/src/components/workstation/Instrument.js
@@ -1,7 +1,14 @@
 import React, {useState, useEffect} from 'react'
 import './Workstation.css'
+import Step from "./step";
 
 const Instrument = props => {
+
+    const steps = []
+
+    for (let i = 0; i < 16; i++) {
+        steps.push({on: true})
+    }
 
     const [name, setName] = useState("Instr " + props.number)
 
@@ -27,7 +34,11 @@ const Instrument = props => {
                     }}
                     onBlur={() => updateName()}
                 />
-
+            </div>
+            <div className="steps">
+                {steps.map((on, index) => {
+                    return <Step on={on} key={index}/>
+                })}
             </div>
         </div>
     )
---
+++ frontend/seqarc/src/components/workstation/step.js
@@ -0,0 +1,15 @@
+import React, {useState, useEffect} from 'react'
+import './Workstation.css'
+
+const Step = props => {
+    return (
+        <div className="stepContainer">
+            <div className="step">
+                <div className="led-on"/>
+            </div>
+        </div>
+    )
+}
+
+export default Step
+
