da9392b5eca369babb6f8ac6a1ea7c46f2399fd9
Sebastian Kihlstrand <sebastian.kihlstrand@hotmail.com>
2019-12-17 09:37:16 +0100

    Issue #24 - ServiceImpl tests now working

--- backend/src/test/java/se/seqarc/samplersequencer/categoryTests/CategoryServiceImplTest.java
+++ backend/src/test/java/se/seqarc/samplersequencer/categoryTests/CategoryServiceImplTest.java
@@ -4,53 +4,35 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.mockito.*;
+import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import se.seqarc.samplersequencer.category.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
-//@SpringBootTest
+@SpringBootTest
 public class CategoryServiceImplTest {
 
-    @Mock
+    @MockBean
     private CategoryRepository categoryRepository;
 
-
+    @Autowired
     private CategoryServiceImpl categoryService;
-
-    @Before
-    public void setup() {
-        categoryService = new CategoryServiceImpl(categoryRepository);
-        MockitoAnnotations.initMocks(this);
-    }
-
-
-   /* @Before
-    public void setUp() {
-        MockitoAnnotations.initMocks(this);
-    }*/
-
-    /*@Test
-    public void testCategory() {
-        String result = categoryService.testCategory();
-
-        assertThat(result).isEqualTo("test-category");
-    }*/
-
-   /* @Before
-    public void setUp() {
-        categoryRepository = Mockito.mock(CategoryRepository.class);
-        categoryService = new CategoryServiceImpl(categoryRepository);
-    }*/
 
     @Test
     public void testGetCategoryByCategory() throws CategoryNotFoundException {
         // GIVEN
         Category bassDrum = new Category();
         bassDrum.setCategory("Bass Drum");
+        bassDrum.setId(1L);
         when(categoryRepository.findCategoryByCategory("Bass Drum"))
                 .thenReturn(java.util.Optional.of(bassDrum));
 
@@ -61,29 +43,30 @@ public class CategoryServiceImplTest {
         Assert.assertEquals(categoryDTO.getCategory(), bassDrum.getCategory());
     }
 
-    /*@Test
+    @Test
     public void testFindAllCategories() {
         // GIVEN
         Category bassDrum = new Category();
         bassDrum.setCategory("Bass Drum");
+        bassDrum.setId(1L);
         Category snare= new Category();
         snare.setCategory("Snare");
+        snare.setId(1L);
         Category clap = new Category();
         clap.setCategory("Clap");
+        clap.setId(1L);
         List<Category> categoryList = new ArrayList<>();
         categoryList.add(bassDrum);
         categoryList.add(snare);
         categoryList.add(clap);
-        Mockito.when(categoryRepository.findAll()).thenReturn(categoryList);
+        when(categoryRepository.findAll()).thenReturn(categoryList);
 
         // WHEN
         List<CategoryDTO> found = categoryService.getAllCategories();
 
         // THEN
-        assertThat(found.get(0)).isEqualTo(categoryList.get(0));
-        assertThat(found.get(1)).isEqualTo(categoryList.get(1));
-        assertThat(found.get(2)).isEqualTo(categoryList.get(2));
-    }*/
+        Assert.assertEquals(categoryList.get(0).getCategory(), found.get(0).getCategory());
+    }
 }
 
 
