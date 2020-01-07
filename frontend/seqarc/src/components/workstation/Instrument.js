022b2455760a98d18a58705bc5251b59307a9ebf
Sebastian Kihlstrand <sebastian.kihlstrand@hotmail.com>
2020-01-07 16:18:06 +0100

    issue#30 - transpose in progress, almost there

--- frontend/seqarc/src/components/workstation/Instrument.js
+++ frontend/seqarc/src/components/workstation/Instrument.js
@@ -32,14 +32,53 @@ const Instrument = props => {
         return steps[index] ? 'led-on' : 'led-off'
     }
 
-    const toggleStepOn = (index) => {
+    const noteValues = [
+    'C2',
+    'Db2',
+    'D2',
+    'Eb2',
+    'E2',
+    'Fb2',
+    'F2',
+    'Gb2',
+    'G2',
+    'Ab2',
+    'A2',
+    'Bb2',
+    'B2',
+    'C3',
+    'C#3',
+    'D3',
+    'D#3',
+    'E3',
+    'E#3',
+    'F3',
+    'F#3',
+    'G3',
+    'G#3',
+    'A3',
+    'A#3',
+    'B3',
+    'C4',
+]
+
+
+    const toggleStepOn = (index, note) => {
         let newArray = [...steps]
         newArray[index] = !steps[index]
         setSteps(newArray)
         if (!steps[index]) {
-            props.addNote(props.index, index, 'C3')
+            props.addNote(props.index, index, noteValues[note])
         } else {
-            props.removeNote(props.index, index, 'C3')
+            props.removeNote(props.index, index, noteValues[note])
+        }
+    }
+
+    const changeNoteValue = (index, note) => {
+        console.log('change note: index: ' + index +', note: ' + note)
+        if (steps[index]) {
+            props.removeNote(props.index, index)
+            props.addNote(props.index, index, noteValues[note])
         }
     }
 
@@ -97,7 +136,9 @@ const Instrument = props => {
                         key={index}
                         index={index}
                         toggleStepOn={toggleStepOn}
+                        changeNoteValue={changeNoteValue}
                         ledStatus={calculateLedStatus(index)}
+                        noteValues={noteValues}
                     />
                 })}
             </div>
--- frontend/seqarc/src/components/workstation/Step.js
+++ frontend/seqarc/src/components/workstation/Step.js
@@ -1,11 +1,50 @@
-import React, {} from 'react'
+import React, {useState, useEffect} from 'react'
 import './Workstation.css'
+import {faCaretUp, faCaretDown} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 const Step = props => {
+
+    const [note, setNote] = useState(13)
+
+    const {changeNoteValue} = props
+
+    useEffect(() => {
+        changeNoteValue(props.index, note)
+    }, [note])
+
     return (
-        <div className={props.index === 0 || props.index === 4 || props.index === 8 || props.index === 12 ? 'step on-beat' : 'step' }
-                onClick={() => props.toggleStepOn(props.index)}>
-            <div className={props.ledStatus}/>
+        <div
+            className={props.index === 0 || props.index === 4 || props.index === 8 || props.index === 12 ? 'step on-beat' : 'step'}>
+            <div className="led-container" onClick={() => props.toggleStepOn(props.index, note)}>
+                <div className={props.ledStatus}/>
+            </div>
+            <div className="note-container">
+                <div className="note-value">{props.noteValues[note]}</div>
+                <div className="note-change-container">
+                    <FontAwesomeIcon
+                        className="note-up"
+                        icon={faCaretUp}
+                        onClick={() => {
+                            if (note < 26) {
+                                setNote(note + 1)
+                            }
+                        }
+                        }
+                    />
+                    <FontAwesomeIcon
+                        className="note-down"
+                        icon={faCaretDown}
+                        onClick={() => {
+                            if (note > 0) {
+                                setNote(note - 1)
+                                props.changeNoteValue(props.index, note)
+                            }
+                        }
+                        }
+                    />
+                </div>
+            </div>
         </div>
     )
 }
