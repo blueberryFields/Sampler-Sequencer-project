b07b9289a45397784d7eba346d54b024b1e5ce12
SebastianKihlstrand <sebastian.kihlstran@hotmail.com>
2020-01-08 11:51:31 +0100

    Update toolbar logo

--- frontend/seqarc/src/components/toolbar/Toolbar.js
+++ frontend/seqarc/src/components/toolbar/Toolbar.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 import Workstation from "../workstation/Workstation";
 import Home from "../home/Home";
 import About from "../about/About";
 import Profile from "../profile/Profile";
 import './Toolbar.css'
+import {faInfinity} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 const toolbar = props => {
     return (
-            <Router>
-                <header className="toolbar">
-                    <nav className="toolbar_navigation">
-                        <div className="toolbar_logo"><Link to="/">SeqArc</Link></div>
-                        <div className="spacer"></div>
-                        <div className="toolbar_navigation_items">
+        <Router>
+            <header className="toolbar">
+                <nav className="toolbar_navigation">
+                    <div className="toolbar_logo">
+                        <Link
+                            to="/">
+                            SeqArc
+                            <FontAwesomeIcon
+                                className="infinity"
+                                icon={faInfinity}/>
+                        </Link>
+                    </div>
+                    <div className="spacer"></div>
+                    <div className="toolbar_navigation_items">
                         <ul className="navbar">
                             <li>
                                 <Link to="/">Home</Link>
                             </li>
-                            <li> 
+                            <li>
                                 <Link to="/about">About</Link>
-                            </li>            
+                            </li>
                             <li>
                                 <Link to="/workstation">Workstation</Link>
                             </li>
@@ -28,25 +38,25 @@ const toolbar = props => {
                                 <Link to="/profile">Profile</Link>
                             </li>
                         </ul>
-                        </div>
-                    </nav>
-                </header>
-                <main className="main">
-                    <Switch>
+                    </div>
+                </nav>
+            </header>
+            <main className="main">
+                <Switch>
                     <Route path="/about">
-                        <About />
+                        <About/>
                     </Route>
                     <Route path="/workstation">
-                        <Workstation />
+                        <Workstation/>
                     </Route>
                     <Route path="/profile">
-                        <Profile />
+                        <Profile/>
                     </Route>
                     <Route path="/">
-                        <Home />
+                        <Home/>
                     </Route>
-                    </Switch>
-                </main>
+                </Switch>
+            </main>
         </Router>
     )
 }
