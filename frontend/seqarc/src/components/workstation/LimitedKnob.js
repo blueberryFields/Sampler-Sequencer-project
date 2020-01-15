4fa7e89397028f5f1a0ecf572b0c72dea39747ca
SebastianKihlstrand <sebastian.kihlstran@hotmail.com>
2020-01-15 12:06:41 +0100

    Revert to working LimitedKnob

--- frontend/seqarc/src/components/workstation/LimitedKnob.js
+++ frontend/seqarc/src/components/workstation/LimitedKnob.js
@@ -1,31 +1,42 @@
-import React, {useState} from "react";
-import {render} from "react-dom";
-import {Knob} from "react-rotary-knob";
+import React from "react";
+import { render } from "react-dom";
+import { Knob } from "react-rotary-knob";
 
-const LimitedKnob = props => {
+// const styles = {
+//     fontFamily: "sans-serif",
+//     textAlign: "center",
+//     marginTop: "6rem"
+// };
 
-    const [value, setValue] = useState(0)
+class LimitedKnob extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            value: 0
+        };
+        this.handleOnChange = this.handleOnChange.bind(this);
+    }
 
-    const handleOnChange = (val) => {
+    handleOnChange(val) {
         //ignore change if distance is greater than defined
         //here we use a distance of 200 because our max value is 1000
         //change if needed
-        const maxDistance = 40;
+        const maxDistance = 20;
         let distance = Math.abs(val - this.state.value);
         if (distance > maxDistance) {
             return;
         } else {
-            setValue(val);
-            props.setPan(Math.round(val))
+            this.setState({ value: val });
+            this.props.setPan(Math.round(val))
         }
     }
+    render() {
+        let { value, ...rest } = this.props;
 
-    // let { value, ...rest } = this.props;
-
-    return (
-        <Knob value={value} onChange={handleOnChange}/>
-    )
-
+        return (
+            <Knob value={this.state.value} onChange={this.handleOnChange} {...rest} />
+        );
+    }
 }
 
 export default LimitedKnob
