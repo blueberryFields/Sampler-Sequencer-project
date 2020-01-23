fa7e2789fe38f527914fc0766cc419d7c74c8237
Adam Hambe <adamhambe@gmail.com>
2020-01-23 14:24:21 +0100

    Cleaned up a little bit

--- frontend/seqarc/src/components/profile/Profile.js
+++ frontend/seqarc/src/components/profile/Profile.js
@@ -1,8 +1,6 @@
 import React, {useEffect, useState, useCallback} from 'react';
 import "./Profile.css"
-import "../images/Doom_Guy.jpg"
 import jwtDecode from 'jwt-decode'
-import { useLocalStorage} from '@rehooks/local-storage';
 import Axios from 'axios-observable';
 
 /* This page will be accessed by logging in.
