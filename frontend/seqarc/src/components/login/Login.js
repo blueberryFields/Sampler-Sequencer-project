9986d96fa1366e944c4a83f750d62005607f6cb2
SebastianKihlstrand <sebastian.kihlstran@hotmail.com>
2020-07-08 11:24:17 +0200

    Do some minor css fixes, make login/register-button tabbable and 'enter-pressable'.

--- frontend/seqarc/src/components/login/Login.js
+++ frontend/seqarc/src/components/login/Login.js
@@ -10,55 +10,64 @@ function Login({ setShowLogin }) {
   const [loginMode, setLoginMode] = useState(true);
 
   const login = useCallback(() => {
-    return Axios.request({
-      method: "post",
-      url: "http://localhost:8080/user/login",
-      data: {
-        username,
-        password,
-      },
-    }).subscribe(
-      (response) => {
-        writeStorage("jwt", response.data);
-        setShowLogin(false);
-      },
-      (error) => {
-        console.log(error);
-        if (error.response.status === 500) {
-          alert("Invalid username and/or password");
+    if (username && password)
+      return Axios.request({
+        method: "post",
+        url: "http://localhost:8080/user/login",
+        data: {
+          username,
+          password,
+        },
+      }).subscribe(
+        (response) => {
+          writeStorage("jwt", response.data);
+          setShowLogin(false);
+        },
+        (error) => {
+          console.log(error);
+          if (error.response.status === 500) {
+            alert("Invalid username and/or password");
+          }
         }
-      }
-    );
+      );
   }, [password, username, setShowLogin]);
 
   const register = useCallback(() => {
-    return Axios.request({
-      method: "post",
-      url: "http://localhost:8080/user/create",
-      data: {
-        username,
-        password,
-        profileDescription: "",
-        roles: ["ROLE_USER"],
-      },
-    }).subscribe(
-      (response) => {
-        setUsername("");
-        setPassword("");
-        setLoginMode(true);
-      },
-      (error) => {
-        console.log(error.response);
-        if (error.response.status === 409) {
-          alert("Username is already taken.\nPlease select a new one.");
+    if (username && password)
+      return Axios.request({
+        method: "post",
+        url: "http://localhost:8080/user/create",
+        data: {
+          username,
+          password,
+          profileDescription: "",
+          roles: ["ROLE_USER"],
+        },
+      }).subscribe(
+        (response) => {
+          setUsername("");
+          setPassword("");
+          setLoginMode(true);
+        },
+        (error) => {
+          console.log(error.response);
+          if (error.response.status === 409) {
+            alert("Username is already taken.\nPlease select a new one.");
+          }
         }
-      }
-    );
+      );
   }, [username, password]);
 
   return (
     <div className="login-container">
-      <div className="login-content">
+      <div
+        className="login-content"
+        onKeyPress={(event) => {
+          if (event.key === "Enter") {
+            login();
+          }
+        }}
+      >
         <input
           className="login-input"
           type="text"
@@ -75,7 +84,7 @@ function Login({ setShowLogin }) {
         />
         {loginMode ? (
           <>
-            <div className="login-button" onClick={login}>
+            <div tabIndex="0" className="login-button" onClick={login}>
               Login
             </div>
             <div>
@@ -88,7 +97,7 @@ function Login({ setShowLogin }) {
           </>
         ) : (
           <>
-            <div className="login-button" onClick={register}>
+            <div tabIndex="0" className="login-button" onClick={register}>
               Signup
             </div>
             <div>
