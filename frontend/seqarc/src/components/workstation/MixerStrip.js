7b4e3564dc5fe641e2b0b37c5513828d887dcd49
Sebastian Kihlstrand <sebastian.kihlstrand@hotmail.com>
2020-07-13 16:28:10 +0200

    Change from css-animation with setInterval to canvas-animation, not sure if this works ideally?

--- frontend/seqarc/src/components/workstation/MixerStrip.js
+++ frontend/seqarc/src/components/workstation/MixerStrip.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState, useRef, useCallback } from "react";
 import * as skins from "react-rotary-knob-skin-pack";
-import LimitedKnobHooks from "./LimitedKnobHooks";
 import useInterval from "../hooks/useInterval";
+import LimitedKnobHooks from "./LimitedKnobHooks";
 
 const MixerStrip = ({
   changePan,
@@ -13,35 +13,82 @@ const MixerStrip = ({
 }) => {
   const [volume, setVolume] = useState(0);
   const [pan, setPan] = useState(0);
-  const [vuMeter, setVuMeter] = useState(0);
-  const [timerDelay, setTimerDelay] = useState(null);
 
+  // const [vuMeter, setVuMeter] = useState(0);
+  // const [timerDelay, setTimerDelay] = useState(null);
+
+  // useEffect(() => {
+  //   if (playing) {
+  //     setTimerDelay(50);
+  //   } else {
+  //     setTimeout(() => {
+  //       setTimerDelay(null);
+  //       setVuMeter(0);
+  //     }, 1000);
+  //   }
+  // }, [playing, setTimerDelay]);
+
+  // useInterval(() => {
+  //   setVuMeter(calcMeterHeight(meter.getLevel()));
+  // }, timerDelay);
+
+  // const calcMeterHeight = (level) => {
+  //   if (level >= -32 && level < 0) {
+  //     return 8 + level / 4;
+  //   } else if (level === 0) {
+  //     return 8;
+  //   } else if (level > 0) {
+  //     return 8 + Math.abs(level / 4);
+  //   } else {
+  //     return 0;
+  //   }
+  // };
+
+  const canvas = useRef(null);
+  let ctx = useRef(null);
   useEffect(() => {
-    if (playing) {
-      setTimerDelay(50);
-    } else {
-      setTimeout(() => {
-        setTimerDelay(null);
-        setVuMeter(0);
-      }, 1000);
-    }
-  }, [playing, setTimerDelay]);
+    console.log("Canvas context effect fired");
+    ctx.current = canvas.current.getContext("2d");
+  }, []);
 
-  useInterval(() => {
-    setVuMeter(calcMeterHeight(meter.getLevel()));
-  }, timerDelay);
+  const meterAnimation = useCallback(() => {
+    clearCanvas();
+    const meterHeight = calcCanvasMeterHeight(meter.getLevel());
+    const meterColor = meterHeight > 128 ? "red" : "green";
+    drawMeter(meterHeight, meterColor);
+    requestAnimationFrame(meterAnimation)
+  }, [meter]);
 
-  const calcMeterHeight = (level) => {
-    if (level >= -32 && level < 0) {
-      return 8 + level / 4;
+  const calcCanvasMeterHeight = (level) => {
+    if (level >= -128 && level < 0) {
+      return 128 + level;
     } else if (level === 0) {
-      return 8;
+      return 128;
     } else if (level > 0) {
-      return 8 + Math.abs(level / 4);
+      return 128 + level;
     } else {
       return 0;
     }
   };
+
+  const drawMeter = (meterHeight, color) => {
+    ctx.current.fillStyle = color;
+    ctx.current.fillRect(
+      0,
+      canvas.current.height - meterHeight,
+      canvas.current.width,
+      meterHeight
+    );
+  };
+
+  const clearCanvas = () => {
+    ctx.current.clearRect(0, 0, canvas.current.width, canvas.current.height);
+  };
+
+  useEffect(() => {
+    console.log("Start effect fired");
+    requestAnimationFrame(meterAnimation);
+  }, [meterAnimation]);
 
   useEffect(() => {
     changePan(index, pan);
@@ -88,16 +135,22 @@ const MixerStrip = ({
         />
       </div>
       <div className="meter-container">
-        <div
+        {/* <div
           className="meter"
           style={{
             height: vuMeter + "rem",
             background: vuMeter > 8 ? "red" : "#66ff66",
           }}
-        />
+        /> */}
+        <canvas ref={canvas} width="8" height="160" />
       </div>
       <div className="volume">{volume + " db"}</div>
-      <div className="mix-strip-pad" onClick={() => triggerInstrument(index)}>
+      <div
+        className="mix-strip-pad"
+        onClick={() => {
+          triggerInstrument(index);
+        }}
+      >
         <div className="mix-strip-number">{index + 1}</div>
       </div>
     </div>
